a52d912b652c04a04fbf0ee532f1bc8c8ee17b67
Marco Zanin <mrczn.bb@gmail.com>
2023-07-01 05:32:55 +0000

    sweet: proprietary-files.txt: add liblocdiagiface.so
    It's needed by GPS logging.
    
    fix: 04-13 16:14:09.598   924   924 E LocSvc_misc_utils: logDlError:129] dlopen error: dlopen failed: library "liblocdiagiface.so" not found
    
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

--- sweet-vendor.mk
+++ sweet-vendor.mk
@@ -674,6 +674,7 @@ PRODUCT_PACKAGES += \
     libloc_socket \
     liblocationservice \
     liblocationservice_glue \
+    liblocdiagiface \
     liblowi_client \
     liblowi_wifihal \
     liblqe \
